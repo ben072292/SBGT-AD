ce651f61b0b920be074890a459305532c905a296
weicongchen <ben072292@gmail.com>
2022-03-05 00:34:42 -0500

    revise

--- run.sh
+++ run.sh
@@ -1,38 +1,38 @@
-../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077  --total-executor-cores 56 --class edu.cwru.bayesgrouptesting.benchmarks.LatticeBenchmark SC22-OP7.jar
+../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077  --total-executor-cores 1 --class edu.cwru.bayesgrouptesting.benchmarks.LatticeBenchmark SC22-OP7.jar
 
-../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 56 --class edu.cwru.bayesgrouptesting.tests.HybridCrossPruningTreeTest SC22-Final.jar 20 LatticeDilution 0.02 0.01 0.01 1 24 0 12 1e-5
-../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 56 --class edu.cwru.bayesgrouptesting.tests.HybridCrossPruningTreeTest SC22-Final.jar 21 LatticeDilution 0.02 0.01 0.01 1 24 0 12 1e-5
-../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 56 --class edu.cwru.bayesgrouptesting.tests.HybridCrossPruningTreeTest SC22-Final.jar 22 LatticeDilution 0.02 0.01 0.01 1 24 0 12 1e-5
-../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 56 --class edu.cwru.bayesgrouptesting.tests.HybridCrossPruningTreeTest SC22-Final.jar 23 LatticeDilution 0.02 0.01 0.01 1 24 0 12 1e-5
-../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 56 --class edu.cwru.bayesgrouptesting.tests.HybridCrossPruningTreeTest SC22-Final.jar 24 LatticeDilution 0.02 0.01 0.01 1 24 0 12 1e-5
-../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 56 --class edu.cwru.bayesgrouptesting.tests.HybridCrossPruningTreeTest SC22-Final.jar 25 LatticeDilution 0.02 0.01 0.01 1 24 0 12 1e-5
+../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 1 --class edu.cwru.bayesgrouptesting.tests.HybridCrossPruningTreeTest SC22-Final.jar 20 LatticeDilution 0.02 0.01 0.01 1 24 0 12 1e-5
+../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 1 --class edu.cwru.bayesgrouptesting.tests.HybridCrossPruningTreeTest SC22-Final.jar 21 LatticeDilution 0.02 0.01 0.01 1 24 0 12 1e-5
+../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 1 --class edu.cwru.bayesgrouptesting.tests.HybridCrossPruningTreeTest SC22-Final.jar 22 LatticeDilution 0.02 0.01 0.01 1 24 0 12 1e-5
+../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 1 --class edu.cwru.bayesgrouptesting.tests.HybridCrossPruningTreeTest SC22-Final.jar 23 LatticeDilution 0.02 0.01 0.01 1 24 0 12 1e-5
+../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 1 --class edu.cwru.bayesgrouptesting.tests.HybridCrossPruningTreeTest SC22-Final.jar 24 LatticeDilution 0.02 0.01 0.01 1 24 0 12 1e-5
+../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 1 --class edu.cwru.bayesgrouptesting.tests.HybridCrossPruningTreeTest SC22-Final.jar 25 LatticeDilution 0.02 0.01 0.01 1 24 0 12 1e-5
 
-../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 56 --class edu.cwru.bayesgrouptesting.tests.HybridTreeTest SC22-Final.jar 20 LatticeDilution 0.02 0.01 0.01 1 10 5
-../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 56 --class edu.cwru.bayesgrouptesting.tests.HybridTreeTest SC22-Final.jar 21 LatticeDilution 0.02 0.01 0.01 1 10 5
-../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 56 --class edu.cwru.bayesgrouptesting.tests.HybridTreeTest SC22-Final.jar 22 LatticeDilution 0.02 0.01 0.01 1 10 5
-../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 56 --class edu.cwru.bayesgrouptesting.tests.HybridTreeTest SC22-Final.jar 23 LatticeDilution 0.02 0.01 0.01 1 10 5
-../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 56 --class edu.cwru.bayesgrouptesting.tests.HybridTreeTest SC22-Final.jar 24 LatticeDilution 0.02 0.01 0.01 1 10 5
-../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 56 --class edu.cwru.bayesgrouptesting.tests.HybridTreeTest SC22-Final.jar 25 LatticeDilution 0.02 0.01 0.01 1 10 5
+../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 1 --class edu.cwru.bayesgrouptesting.tests.HybridTreeTest SC22-Final.jar 20 LatticeDilution 0.02 0.01 0.01 1 10 5
+../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 1 --class edu.cwru.bayesgrouptesting.tests.HybridTreeTest SC22-Final.jar 21 LatticeDilution 0.02 0.01 0.01 1 10 5
+../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 1 --class edu.cwru.bayesgrouptesting.tests.HybridTreeTest SC22-Final.jar 22 LatticeDilution 0.02 0.01 0.01 1 10 5
+../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 1 --class edu.cwru.bayesgrouptesting.tests.HybridTreeTest SC22-Final.jar 23 LatticeDilution 0.02 0.01 0.01 1 10 5
+../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 1 --class edu.cwru.bayesgrouptesting.tests.HybridTreeTest SC22-Final.jar 24 LatticeDilution 0.02 0.01 0.01 1 10 5
+../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 1 --class edu.cwru.bayesgrouptesting.tests.HybridTreeTest SC22-Final.jar 25 LatticeDilution 0.02 0.01 0.01 1 10 5
 
-../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 56 --class edu.cwru.bayesgrouptesting.tests.SingleTreeTest SC22-Final.jar 20 LatticeDilution 0.02 0.01 0.01 1 15
-../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 56 --class edu.cwru.bayesgrouptesting.tests.SingleTreeTest SC22-Final.jar 21 LatticeDilution 0.02 0.01 0.01 1 15
-../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 56 --class edu.cwru.bayesgrouptesting.tests.SingleTreeTest SC22-Final.jar 22 LatticeDilution 0.02 0.01 0.01 1 15
-../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 56 --class edu.cwru.bayesgrouptesting.tests.SingleTreeTest SC22-Final.jar 23 LatticeDilution 0.02 0.01 0.01 1 15
-../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 56 --class edu.cwru.bayesgrouptesting.tests.SingleTreeTest SC22-Final.jar 24 LatticeDilution 0.02 0.01 0.01 1 15
-../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 56 --class edu.cwru.bayesgrouptesting.tests.SingleTreeTest SC22-Final.jar 25 LatticeDilution 0.02 0.01 0.01 1 15
+../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 1 --class edu.cwru.bayesgrouptesting.tests.SingleTreeTest SC22-Final.jar 20 LatticeDilution 0.02 0.01 0.01 1 15
+../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 1 --class edu.cwru.bayesgrouptesting.tests.SingleTreeTest SC22-Final.jar 21 LatticeDilution 0.02 0.01 0.01 1 15
+../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 1 --class edu.cwru.bayesgrouptesting.tests.SingleTreeTest SC22-Final.jar 22 LatticeDilution 0.02 0.01 0.01 1 15
+../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 1 --class edu.cwru.bayesgrouptesting.tests.SingleTreeTest SC22-Final.jar 23 LatticeDilution 0.02 0.01 0.01 1 15
+../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 1 --class edu.cwru.bayesgrouptesting.tests.SingleTreeTest SC22-Final.jar 24 LatticeDilution 0.02 0.01 0.01 1 15
+../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 1 --class edu.cwru.bayesgrouptesting.tests.SingleTreeTest SC22-Final.jar 25 LatticeDilution 0.02 0.01 0.01 1 15
 
-../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 56 --class edu.cwru.bayesgrouptesting.tests.MultiTreeTest SC22-Final.jar 17 LatticeDilution 0.02 0.01 0.01 1 10
-../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 56 --class edu.cwru.bayesgrouptesting.tests.MultiTreeTest SC22-Final.jar 18 LatticeDilution 0.02 0.01 0.01 1 10
-../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 56 --class edu.cwru.bayesgrouptesting.tests.MultiTreeTest SC22-Final.jar 19 LatticeDilution 0.02 0.01 0.01 1 10
-../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 56 --class edu.cwru.bayesgrouptesting.tests.MultiTreeTest SC22-Final.jar 20 LatticeDilution 0.02 0.01 0.01 1 10
-../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 56 --class edu.cwru.bayesgrouptesting.tests.MultiTreeTest SC22-Final.jar 21 LatticeDilution 0.02 0.01 0.01 1 10
-../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 56 --class edu.cwru.bayesgrouptesting.tests.MultiTreeTest SC22-Final.jar 22 LatticeDilution 0.02 0.01 0.01 1 10
+../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 1 --class edu.cwru.bayesgrouptesting.tests.MultiTreeTest SC22-Final.jar 17 LatticeDilution 0.02 0.01 0.01 1 10
+../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 1 --class edu.cwru.bayesgrouptesting.tests.MultiTreeTest SC22-Final.jar 18 LatticeDilution 0.02 0.01 0.01 1 10
+../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 1 --class edu.cwru.bayesgrouptesting.tests.MultiTreeTest SC22-Final.jar 19 LatticeDilution 0.02 0.01 0.01 1 10
+../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 1 --class edu.cwru.bayesgrouptesting.tests.MultiTreeTest SC22-Final.jar 20 LatticeDilution 0.02 0.01 0.01 1 10
+../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 1 --class edu.cwru.bayesgrouptesting.tests.MultiTreeTest SC22-Final.jar 21 LatticeDilution 0.02 0.01 0.01 1 10
+../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077 --total-executor-cores 1 --class edu.cwru.bayesgrouptesting.tests.MultiTreeTest SC22-Final.jar 22 LatticeDilution 0.02 0.01 0.01 1 10
 
-../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077  --total-executor-cores 56 --class edu.cwru.bayesgrouptesting.benchmarks.LatticeBenchmark SC22-OP6.jar
-../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077  --total-executor-cores 56 --class edu.cwru.bayesgrouptesting.benchmarks.LatticeBenchmark SC22-OP5.jar
-../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077  --total-executor-cores 56 --class edu.cwru.bayesgrouptesting.benchmarks.LatticeBenchmark SC22-OP4.jar
-../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077  --total-executor-cores 56 --class edu.cwru.bayesgrouptesting.benchmarks.LatticeBenchmark SC22-OP3.jar
-../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077  --total-executor-cores 56 --class edu.cwru.bayesgrouptesting.benchmarks.LatticeBenchmark SC22-OP2.jar
-../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077  --total-executor-cores 56 --class edu.cwru.bayesgrouptesting.benchmarks.LatticeBenchmark SC22-OP2.jar
+../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077  --total-executor-cores 1 --class edu.cwru.bayesgrouptesting.benchmarks.LatticeBenchmark SC22-OP6.jar
+../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077  --total-executor-cores 1 --class edu.cwru.bayesgrouptesting.benchmarks.LatticeBenchmark SC22-OP5.jar
+../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077  --total-executor-cores 1 --class edu.cwru.bayesgrouptesting.benchmarks.LatticeBenchmark SC22-OP4.jar
+../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077  --total-executor-cores 1 --class edu.cwru.bayesgrouptesting.benchmarks.LatticeBenchmark SC22-OP3.jar
+../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077  --total-executor-cores 1 --class edu.cwru.bayesgrouptesting.benchmarks.LatticeBenchmark SC22-OP2.jar
+../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --master spark://node013:7077  --total-executor-cores 1 --class edu.cwru.bayesgrouptesting.benchmarks.LatticeBenchmark SC22-OP2.jar
 
 
