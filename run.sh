b17a3716ef55e76086d34fb3b7200eebfb9db5a0
weicongchen <ben072292@gmail.com>
2022-03-04 23:33:30 -0500

    benchmark

---
+++ run.sh
@@ -0,0 +1,38 @@
+spark-submit --master node013  --class edu.cwru.bayesgrouptesting.benchmarks.LatticeBenchmark SC22-OP7.jar
+
+spark-submit --master node013 --class edu.cwru.bayesgrouptesting.tests.HybridCrossPruningTreeTest SC22-Final.jar 20 LatticeDilution 0.02 0.01 0.01 1 24 0 12 1e-5
+spark-submit --master node013 --class edu.cwru.bayesgrouptesting.tests.HybridCrossPruningTreeTest SC22-Final.jar 21 LatticeDilution 0.02 0.01 0.01 1 24 0 12 1e-5
+spark-submit --master node013 --class edu.cwru.bayesgrouptesting.tests.HybridCrossPruningTreeTest SC22-Final.jar 22 LatticeDilution 0.02 0.01 0.01 1 24 0 12 1e-5
+spark-submit --master node013 --class edu.cwru.bayesgrouptesting.tests.HybridCrossPruningTreeTest SC22-Final.jar 23 LatticeDilution 0.02 0.01 0.01 1 24 0 12 1e-5
+spark-submit --master node013 --class edu.cwru.bayesgrouptesting.tests.HybridCrossPruningTreeTest SC22-Final.jar 24 LatticeDilution 0.02 0.01 0.01 1 24 0 12 1e-5
+spark-submit --master node013 --class edu.cwru.bayesgrouptesting.tests.HybridCrossPruningTreeTest SC22-Final.jar 25 LatticeDilution 0.02 0.01 0.01 1 24 0 12 1e-5
+
+spark-submit --master node013 --class edu.cwru.bayesgrouptesting.tests.HybridTreeTest SC22-Final.jar 20 LatticeDilution 0.02 0.01 0.01 1 10 5
+spark-submit --master node013 --class edu.cwru.bayesgrouptesting.tests.HybridTreeTest SC22-Final.jar 21 LatticeDilution 0.02 0.01 0.01 1 10 5
+spark-submit --master node013 --class edu.cwru.bayesgrouptesting.tests.HybridTreeTest SC22-Final.jar 22 LatticeDilution 0.02 0.01 0.01 1 10 5
+spark-submit --master node013 --class edu.cwru.bayesgrouptesting.tests.HybridTreeTest SC22-Final.jar 23 LatticeDilution 0.02 0.01 0.01 1 10 5
+spark-submit --master node013 --class edu.cwru.bayesgrouptesting.tests.HybridTreeTest SC22-Final.jar 24 LatticeDilution 0.02 0.01 0.01 1 10 5
+spark-submit --master node013 --class edu.cwru.bayesgrouptesting.tests.HybridTreeTest SC22-Final.jar 25 LatticeDilution 0.02 0.01 0.01 1 10 5
+
+spark-submit --master node013 --class edu.cwru.bayesgrouptesting.tests.SingleTreeTest SC22-Final.jar 20 LatticeDilution 0.02 0.01 0.01 1 15
+spark-submit --master node013 --class edu.cwru.bayesgrouptesting.tests.SingleTreeTest SC22-Final.jar 21 LatticeDilution 0.02 0.01 0.01 1 15
+spark-submit --master node013 --class edu.cwru.bayesgrouptesting.tests.SingleTreeTest SC22-Final.jar 22 LatticeDilution 0.02 0.01 0.01 1 15
+spark-submit --master node013 --class edu.cwru.bayesgrouptesting.tests.SingleTreeTest SC22-Final.jar 23 LatticeDilution 0.02 0.01 0.01 1 15
+spark-submit --master node013 --class edu.cwru.bayesgrouptesting.tests.SingleTreeTest SC22-Final.jar 24 LatticeDilution 0.02 0.01 0.01 1 15
+spark-submit --master node013 --class edu.cwru.bayesgrouptesting.tests.SingleTreeTest SC22-Final.jar 25 LatticeDilution 0.02 0.01 0.01 1 15
+
+spark-submit --master node013 --class edu.cwru.bayesgrouptesting.tests.MultiTreeTest SC22-Final.jar 17 LatticeDilution 0.02 0.01 0.01 1 10
+spark-submit --master node013 --class edu.cwru.bayesgrouptesting.tests.MultiTreeTest SC22-Final.jar 18 LatticeDilution 0.02 0.01 0.01 1 10
+spark-submit --master node013 --class edu.cwru.bayesgrouptesting.tests.MultiTreeTest SC22-Final.jar 19 LatticeDilution 0.02 0.01 0.01 1 10
+spark-submit --master node013 --class edu.cwru.bayesgrouptesting.tests.MultiTreeTest SC22-Final.jar 20 LatticeDilution 0.02 0.01 0.01 1 10
+spark-submit --master node013 --class edu.cwru.bayesgrouptesting.tests.MultiTreeTest SC22-Final.jar 21 LatticeDilution 0.02 0.01 0.01 1 10
+spark-submit --master node013 --class edu.cwru.bayesgrouptesting.tests.MultiTreeTest SC22-Final.jar 22 LatticeDilution 0.02 0.01 0.01 1 10
+
+spark-submit --master node013  --class edu.cwru.bayesgrouptesting.benchmarks.LatticeBenchmark SC22-OP6.jar
+spark-submit --master node013  --class edu.cwru.bayesgrouptesting.benchmarks.LatticeBenchmark SC22-OP5.jar
+spark-submit --master node013  --class edu.cwru.bayesgrouptesting.benchmarks.LatticeBenchmark SC22-OP4.jar
+spark-submit --master node013  --class edu.cwru.bayesgrouptesting.benchmarks.LatticeBenchmark SC22-OP3.jar
+spark-submit --master node013  --class edu.cwru.bayesgrouptesting.benchmarks.LatticeBenchmark SC22-OP2.jar
+spark-submit --master node013  --class edu.cwru.bayesgrouptesting.benchmarks.LatticeBenchmark SC22-OP2.jar
+
+
