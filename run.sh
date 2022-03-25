5dd41b3f3f37c269ac4c8d1fa3bf813c726275bc
weicongchen <ben072292@gmail.com>
2022-03-24 20:34:30 -0400

    new

--- run.sh
+++ run.sh
@@ -11,44 +11,44 @@ CPUS_PER_TASK=$2
 # $SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.benchmarks.LatticeBenchmark SC22-OP2.jar
 # $SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.benchmarks.LatticeBenchmark SC22-OP1.jar
 
-$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.HybridCrossPruningTreeTest SC22-Final.jar 15 LatticeDilution 0.02 0.01 0.01 1 24 0 12 1e-5 $CPUS_PER_TASK
-$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.HybridCrossPruningTreeTest SC22-Final.jar 16 LatticeDilution 0.02 0.01 0.01 1 24 0 12 1e-5 $CPUS_PER_TASK
-$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.HybridCrossPruningTreeTest SC22-Final.jar 17 LatticeDilution 0.02 0.01 0.01 1 24 0 12 1e-5 $CPUS_PER_TASK
-$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.HybridCrossPruningTreeTest SC22-Final.jar 18 LatticeDilution 0.02 0.01 0.01 1 24 0 12 1e-5 $CPUS_PER_TASK
-$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.HybridCrossPruningTreeTest SC22-Final.jar 19 LatticeDilution 0.02 0.01 0.01 1 24 0 12 1e-5 $CPUS_PER_TASK
-$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.HybridCrossPruningTreeTest SC22-Final.jar 20 LatticeDilution 0.02 0.01 0.01 1 24 0 12 1e-5 $CPUS_PER_TASK
-$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.HybridCrossPruningTreeTest SC22-Final.jar 21 LatticeDilution 0.02 0.01 0.01 1 24 0 12 1e-5 $CPUS_PER_TASK
-$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.HybridCrossPruningTreeTest SC22-Final.jar 22 LatticeDilution 0.02 0.01 0.01 1 24 0 12 1e-5 $CPUS_PER_TASK
-$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.HybridCrossPruningTreeTest SC22-Final.jar 23 LatticeDilution 0.02 0.01 0.01 1 24 0 12 1e-5 $CPUS_PER_TASK
-$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.HybridCrossPruningTreeTest SC22-Final.jar 24 LatticeDilution 0.02 0.01 0.01 1 24 0 12 1e-5 $CPUS_PER_TASK
-$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.HybridCrossPruningTreeTest SC22-Final.jar 25 LatticeDilution 0.02 0.01 0.01 1 24 0 12 1e-5 $CPUS_PER_TASK
+$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.HybridCrossPruningTreeTest SC22-MT.jar 15 LatticeDilution 0.02 0.01 0.01 1 24 0 12 1e-5 $CPUS_PER_TASK
+$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.HybridCrossPruningTreeTest SC22-MT.jar 16 LatticeDilution 0.02 0.01 0.01 1 24 0 12 1e-5 $CPUS_PER_TASK
+$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.HybridCrossPruningTreeTest SC22-MT.jar 17 LatticeDilution 0.02 0.01 0.01 1 24 0 12 1e-5 $CPUS_PER_TASK
+$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.HybridCrossPruningTreeTest SC22-MT.jar 18 LatticeDilution 0.02 0.01 0.01 1 24 0 12 1e-5 $CPUS_PER_TASK
+$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.HybridCrossPruningTreeTest SC22-MT.jar 19 LatticeDilution 0.02 0.01 0.01 1 24 0 12 1e-5 $CPUS_PER_TASK
+$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.HybridCrossPruningTreeTest SC22-MT.jar 20 LatticeDilution 0.02 0.01 0.01 1 24 0 12 1e-5 $CPUS_PER_TASK
+$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.HybridCrossPruningTreeTest SC22-MT.jar 21 LatticeDilution 0.02 0.01 0.01 1 24 0 12 1e-5 $CPUS_PER_TASK
+$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.HybridCrossPruningTreeTest SC22-MT.jar 22 LatticeDilution 0.02 0.01 0.01 1 24 0 12 1e-5 $CPUS_PER_TASK
+$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.HybridCrossPruningTreeTest SC22-MT.jar 23 LatticeDilution 0.02 0.01 0.01 1 24 0 12 1e-5 $CPUS_PER_TASK
+$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.HybridCrossPruningTreeTest SC22-MT.jar 24 LatticeDilution 0.02 0.01 0.01 1 24 0 12 1e-5 $CPUS_PER_TASK
+$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.HybridCrossPruningTreeTest SC22-MT.jar 25 LatticeDilution 0.02 0.01 0.01 1 24 0 12 1e-5 $CPUS_PER_TASK
 
-# $SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.HybridTreeTest SC22-Final.jar 18 LatticeDilution 0.02 0.01 0.01 1 20 4
-# $SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.HybridTreeTest SC22-Final.jar 19 LatticeDilution 0.02 0.01 0.01 1 20 4
-# $SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.HybridTreeTest SC22-Final.jar 20 LatticeDilution 0.02 0.01 0.01 1 20 4
-# $SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.HybridTreeTest SC22-Final.jar 21 LatticeDilution 0.02 0.01 0.01 1 20 4
-# $SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.HybridTreeTest SC22-Final.jar 22 LatticeDilution 0.02 0.01 0.01 1 20 4
-# $SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.HybridTreeTest SC22-Final.jar 23 LatticeDilution 0.02 0.01 0.01 1 20 4
+# $SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.HybridTreeTest SC22-MT.jar 18 LatticeDilution 0.02 0.01 0.01 1 20 4
+# $SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.HybridTreeTest SC22-MT.jar 19 LatticeDilution 0.02 0.01 0.01 1 20 4
+# $SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.HybridTreeTest SC22-MT.jar 20 LatticeDilution 0.02 0.01 0.01 1 20 4
+# $SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.HybridTreeTest SC22-MT.jar 21 LatticeDilution 0.02 0.01 0.01 1 20 4
+# $SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.HybridTreeTest SC22-MT.jar 22 LatticeDilution 0.02 0.01 0.01 1 20 4
+# $SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.HybridTreeTest SC22-MT.jar 23 LatticeDilution 0.02 0.01 0.01 1 20 4
 
-$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.SingleTreeTest SC22-Final.jar 15 LatticeDilution 0.02 0.01 0.01 1 24 $CPUS_PER_TASK
-$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.SingleTreeTest SC22-Final.jar 16 LatticeDilution 0.02 0.01 0.01 1 24 $CPUS_PER_TASK
-$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.SingleTreeTest SC22-Final.jar 17 LatticeDilution 0.02 0.01 0.01 1 24 $CPUS_PER_TASK
-$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.SingleTreeTest SC22-Final.jar 18 LatticeDilution 0.02 0.01 0.01 1 24 $CPUS_PER_TASK
-$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.SingleTreeTest SC22-Final.jar 19 LatticeDilution 0.02 0.01 0.01 1 24 $CPUS_PER_TASK
-$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.SingleTreeTest SC22-Final.jar 20 LatticeDilution 0.02 0.01 0.01 1 24 $CPUS_PER_TASK
-$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.SingleTreeTest SC22-Final.jar 21 LatticeDilution 0.02 0.01 0.01 1 24 $CPUS_PER_TASK
-$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.SingleTreeTest SC22-Final.jar 22 LatticeDilution 0.02 0.01 0.01 1 24 $CPUS_PER_TASK
-$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.SingleTreeTest SC22-Final.jar 23 LatticeDilution 0.02 0.01 0.01 1 24 $CPUS_PER_TASK
+$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.SingleTreeTest SC22-MT.jar 15 LatticeDilution 0.02 0.01 0.01 1 24 $CPUS_PER_TASK
+$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.SingleTreeTest SC22-MT.jar 16 LatticeDilution 0.02 0.01 0.01 1 24 $CPUS_PER_TASK
+$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.SingleTreeTest SC22-MT.jar 17 LatticeDilution 0.02 0.01 0.01 1 24 $CPUS_PER_TASK
+$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.SingleTreeTest SC22-MT.jar 18 LatticeDilution 0.02 0.01 0.01 1 24 $CPUS_PER_TASK
+$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.SingleTreeTest SC22-MT.jar 19 LatticeDilution 0.02 0.01 0.01 1 24 $CPUS_PER_TASK
+$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.SingleTreeTest SC22-MT.jar 20 LatticeDilution 0.02 0.01 0.01 1 24 $CPUS_PER_TASK
+$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.SingleTreeTest SC22-MT.jar 21 LatticeDilution 0.02 0.01 0.01 1 24 $CPUS_PER_TASK
+$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.SingleTreeTest SC22-MT.jar 22 LatticeDilution 0.02 0.01 0.01 1 24 $CPUS_PER_TASK
+$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.SingleTreeTest SC22-MT.jar 23 LatticeDilution 0.02 0.01 0.01 1 24 $CPUS_PER_TASK
 
 
-$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.MultiTreeTest SC22-Final.jar 10 LatticeDilution 0.02 0.01 0.01 1 24
-$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.MultiTreeTest SC22-Final.jar 11 LatticeDilution 0.02 0.01 0.01 1 24
-$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.MultiTreeTest SC22-Final.jar 12 LatticeDilution 0.02 0.01 0.01 1 24
-$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.MultiTreeTest SC22-Final.jar 13 LatticeDilution 0.02 0.01 0.01 1 24
-$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.MultiTreeTest SC22-Final.jar 14 LatticeDilution 0.02 0.01 0.01 1 24
-$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.MultiTreeTest SC22-Final.jar 15 LatticeDilution 0.02 0.01 0.01 1 24
-$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.MultiTreeTest SC22-Final.jar 16 LatticeDilution 0.02 0.01 0.01 1 24
-$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.MultiTreeTest SC22-Final.jar 17 LatticeDilution 0.02 0.01 0.01 1 24
+$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.MultiTreeTest SC22-MT.jar 10 LatticeDilution 0.02 0.01 0.01 1 24
+$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.MultiTreeTest SC22-MT.jar 11 LatticeDilution 0.02 0.01 0.01 1 24
+$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.MultiTreeTest SC22-MT.jar 12 LatticeDilution 0.02 0.01 0.01 1 24
+$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.MultiTreeTest SC22-MT.jar 13 LatticeDilution 0.02 0.01 0.01 1 24
+$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.MultiTreeTest SC22-MT.jar 14 LatticeDilution 0.02 0.01 0.01 1 24
+$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.MultiTreeTest SC22-MT.jar 15 LatticeDilution 0.02 0.01 0.01 1 24
+$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.MultiTreeTest SC22-MT.jar 16 LatticeDilution 0.02 0.01 0.01 1 24
+$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.tests.MultiTreeTest SC22-MT.jar 17 LatticeDilution 0.02 0.01 0.01 1 24
 
 # mkdir Final
 # mv *.csv Final
