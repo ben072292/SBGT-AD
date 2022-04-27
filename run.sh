81cd5db8a62581925c25a6517af3ec4bfe55468a
weicongchen <ben072292@gmail.com>
2022-04-26 21:15:39 -0400

    new

--- run.sh
+++ run.sh
@@ -2,7 +2,13 @@
 
 SPARK_HOME=$1
 CPUS_PER_TASK=$2
-$SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.benchmarks.LatticeBenchmark SC22-OP8.jar $CPUS_PER_TASK
+$SPARK_HOME/bin/spark-submit --class edu.cwru.csds.sprt.experiments.BatchSimulation SPRT_Java-0.0.1-SNAPSHOT.jar 1 4
+$SPARK_HOME/bin/spark-submit --class edu.cwru.csds.sprt.experiments.BatchSimulation SPRT_Java-0.0.1-SNAPSHOT.jar 1 8
+$SPARK_HOME/bin/spark-submit --class edu.cwru.csds.sprt.experiments.BatchSimulation SPRT_Java-0.0.1-SNAPSHOT.jar 1 16
+$SPARK_HOME/bin/spark-submit --class edu.cwru.csds.sprt.experiments.BatchSimulation SPRT_Java-0.0.1-SNAPSHOT.jar 1 32
+$SPARK_HOME/bin/spark-submit --class edu.cwru.csds.sprt.experiments.BatchSimulation SPRT_Java-0.0.1-SNAPSHOT.jar 1 64
+$SPARK_HOME/bin/spark-submit --class edu.cwru.csds.sprt.experiments.BatchSimulation SPRT_Java-0.0.1-SNAPSHOT.jar 1 128
+$SPARK_HOME/bin/spark-submit --class edu.cwru.csds.sprt.experiments.BatchSimulation SPRT_Java-0.0.1-SNAPSHOT.jar 1 256
 # $SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.benchmarks.LatticeBenchmark SC22-OP7.jar
 # $SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.benchmarks.LatticeBenchmark SC22-OP6.jar
 # $SPARK_HOME/bin/spark-submit --class edu.cwru.bayesgrouptesting.benchmarks.LatticeBenchmark SC22-OP6.jar
